7371cde4fdc60df193d2cb86f12a2bd40ed637f6
Mario Gemoll <mario@appliedblockchain.com>
2022-11-23 19:16:07 +0000

    Set encoding on upload

--- site/upload.sh
+++ site/upload.sh
@@ -1,6 +1,6 @@
 for f in *.html
 do
-  aws s3 cp $f s3://$1/${f%%.*} --content-type "text/html"
+  aws s3 cp $f s3://$1/${f%%.*} --content-type "text/html" --content-encoding "utf8"
 done
 
 for f in *.js *.css
